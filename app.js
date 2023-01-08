d9f935580023b5dbefe3df008d1817f07fa26a38
jose acevedo <joseacevedo410@gmail.com>
2023-01-08 04:41:01 -0300

    se crean archivos de registro y login

--- app.js
+++ app.js
@@ -7,5 +7,7 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')))
+app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'login.html')))
+app.get('/registro', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'registro.html')))
 
 app.listen(port, () => `servidor funcionando en http://localhost:$(port)`)
