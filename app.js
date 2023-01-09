025b3f6b29f878701c76787acf2837f96736d592
jose acevedo <joseacevedo410@gmail.com>
2023-01-09 05:52:12 -0300

    se crean formularios en login y registro

--- app.js
+++ app.js
@@ -9,5 +9,6 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')))
 app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'login.html')))
 app.get('/registro', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'registro.html')))
+app.get('/misCompras', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'misCompras.html')))
 
 app.listen(port, () => `servidor funcionando en http://localhost:$(port)`)
